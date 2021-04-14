7731ab8de12c1911d9f12c5d4e06b7f1d46c591d
tombonan <tombonan018@gmail.com>
2021-04-13 22:18:24 -0600

    Add a contact page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="page-container contact-container">
+      <h1 className="landing-text">Contact.</h1>
+      <h3 className="landing-desc">The best way to reach out is via <a href="mailto:hello@tombonan.me">email</a></h3>
+    </div>
+  );
+}
+
+export default Contact;
